60bca7acd110b8a63011fcdc9bdd5c36479d415f
Gabriel Damour <gabriel.damour@info2007.net>
2018-02-23 06:28:58 +1100

    service-worker 2: add script

---
+++ service-worker.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    // TODO - 3.1: Add install and activate event listeners
+
+    // TODO - 3.3: Add a comment to change the service worker
+
+    // TODO - 4: Add fetch listener
+
+})();
