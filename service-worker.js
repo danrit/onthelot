e7619c484de09168aca61d241ea0ff3146f2b66d
Gabriel Damour <gabriel.damour@info2007.net>
2018-02-23 06:40:36 +1100

    service-worker 3: add fetch event

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,21 @@
 (function() {
     'use strict';
 
-    // TODO - 3.1: Add install and activate event listeners
+    self.addEventListener('install', function(event) {
+        console.log('Service worker installing...');
+        // just for test purpose?
+        // For the rest of the lab, we can now test new service worker code without manually unregistering the service worker.
+        self.skipWaiting();
+    });
 
-    // TODO - 3.3: Add a comment to change the service worker
+    self.addEventListener('activate', function(event) {
+        console.log('Service worker activating...');
+    });
 
-    // TODO - 4: Add fetch listener
+    // I'm a new service worker
+
+    self.addEventListener('fetch', function(event) {
+        console.log('Fetching:', event.request.url);
+    });
 
 })();
